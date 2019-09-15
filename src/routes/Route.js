657d14e86ac97d06c5d45956c41b6c1fb2ee23ed
José Rafael <zerafael1@gmail.com>
2019-09-14 20:33:59 -0400

    Persist auth data

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -5,12 +5,10 @@ import { Route, Redirect } from 'react-router-dom';
 import AuthLayout from '~/pages/_layout/auth';
 import DefaultLayout from '~/pages/_layout/default';
 
-import store from '~/store';
+import { store } from '~/store';
 
 function RouteWraper({ component: Component, isPrivate, ...rest }) {
   const { signed } = store.getState().auth;
-
-  console.tron.log(signed);
 
   if (!signed && isPrivate) {
     return <Redirect to="/" />;
